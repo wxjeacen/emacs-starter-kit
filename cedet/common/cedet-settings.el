2987d4c0055d9f6b0272ee63b0255584df01497e
Devil Wang <wxjeacen@gmail.com>
2011-07-29 13:43:29 +0800

    Devil merge from sabayon system

---
+++ cedet/common/cedet-settings.el
@@ -0,0 +1,34 @@
+;; -*- Emacs-Lisp -*-
+
+;; Time-stamp: <2010-08-28 21:03:22 Saturday by taoshanwen>
+
+;; cedet1.0pre6 is conflict with which-func
+;; after require cedet, which-func cann't work
+
+;; http://emacser.com/install-cedet.htm
+;; http://emacser.com/cedet.htm
+
+(require 'util)
+(require 'cedet)
+(require 'cedet-eieio-settings)
+(require 'cedet-ede-settings)
+(require 'cedet-cogre-settings)
+(require 'cedet-semantic-settings)
+(require 'cedet-srecode-settings)
+(require 'cedet-speedbar-settings)
+
+;; 用pulse实现Emacs的淡入淡出效果
+;; http://emacser.com/pulse.htm
+;;(require 'pulse-settings)
+
+;;;###autoload
+(defun cedet-settings-4-info ()
+  "`cedet' settings for `info'."
+  (info-initialize)
+  (dolist (package `("cogre" "common" "ede" "eieio" "semantic/doc" "speedbar" "srecode"))
+    (add-to-list 'Info-directory-list (concat dotfiles-dir "cedet/" package "/"))))
+
+(eval-after-load "info"
+  `(cedet-settings-4-info))
+
+(provide 'cedet-settings)
--- init.el
+++ init.el
@@ -58,7 +58,7 @@
 (require 'starter-kit-registers)
 (require 'starter-kit-eshell)
 (require 'starter-kit-lisp)
-(require 'starter-kit-perl)
+;;(require 'starter-kit-perl)
 (require 'starter-kit-ruby)
 (require 'starter-kit-js)
 
@@ -72,8 +72,10 @@
 (add-to-list 'load-path user-specific-dir)
 
 (if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
 (if (file-exists-p user-specific-dir)
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
-(if (file-exists-p user-specific-config) (load user-specific-config))
 
 ;;; init.el ends here
+
+
